051cff02395291f1e6bf248c8c724ce7a3b97723
SeeLog <seelog693@gmail.com>
2023-04-16 20:15:15 +0900

    [update] bigger font

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,6 @@ import {
   Box,
   Button,
   Flex,
-  Menu,
-  MenuButton,
   Stack,
   useColorMode,
   useColorModeValue,
@@ -15,9 +13,9 @@ const Header: React.FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const toggle = useCallback(() => {
     toggleColorMode();
-    const styleEl = document.createElement('style');
+    const styleEl = document.createElement("style");
     const cssText = document.createTextNode(
-      'html * { transition: color, background-color 0.3s ease-out!important }',
+      "html * { transition: color, background-color 0.3s ease-out!important }"
     );
     styleEl.appendChild(cssText);
     document.head.appendChild(styleEl);
@@ -37,13 +35,17 @@ const Header: React.FC = () => {
       w="100%"
       zIndex={100}
     >
-      <Flex h={16} alignItems="center" justifyContent="space-between">
-        <Box color={colorMode === "light" ? "white" : "white"} as="h1" fontSize={24}>
+      <Flex h="24" alignItems="center" justifyContent="space-between">
+        <Box
+          color={colorMode === "light" ? "white" : "white"}
+          as="h1"
+          fontSize="3xl"
+        >
           seelog.me
         </Box>
         <Stack direction="row">
-          <Button onClick={toggle}>
-            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          <Button onClick={toggle} size="lg">
+            {colorMode === "light" ? <MoonIcon boxSize="6" /> : <SunIcon boxSize="6" />}
           </Button>
         </Stack>
       </Flex>
