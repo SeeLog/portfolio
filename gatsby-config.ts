1f8c42dd318d69a25189fc722069d0c3b04b1096
SeeLog <seelog693@gmail.com>
2023-04-23 03:24:54 +0900

    [fix] remove chakra plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -50,26 +50,6 @@ const config: GatsbyConfig = {
       },
       __key: "images",
     },
-    {
-      resolve: "@chakra-ui/gatsby-plugin",
-      options: {
-        /**
-         * @property {boolean} [resetCSS=true]
-         * if false, this plugin will not use `<CSSReset />
-         */
-        resetCSS: true,
-        /**
-         * @property {boolean} [isUsingColorMode=true]
-         * if false, this plugin will not use <ColorModeProvider />
-         */
-        isUsingColorMode: true,
-        /**
-         * @property {boolean} [isBaseProvider=false]
-         * if true, will render `<ChakraBaseProvider>`
-         */
-        isBaseProvider: false,
-      },
-    },
   ],
 };
 
